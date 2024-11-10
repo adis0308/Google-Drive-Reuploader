50c66ed3fc002b0ce79df9c6b2dfd4703db7e0ac
adis0308 <adis.fireball@hotmail.co.id>
2024-11-10 16:10:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ refresh_token_file = "refresh_token.txt"
 access_token_file = "access_token.json"
 upload_max_retries = 15
 upload_retry_delay = 5
+ffprobe_path = os.path.join(current_dir, "ffprobe")
+ffmpeg_path = os.path.join(current_dir, "ffmpeg")
 ffmpeg_threads = '0'
 video_extensions = ('.mp4', '.mkv')  # Tambahkan ekstensi lain jika perlu
 data_file = "data.txt"
@@ -137,7 +139,7 @@ def remove_words(text, words_to_remove):
 # Fungsi untuk mengecek subtitle pada video dengan FFmpeg
 def has_subtitle(input_video_path):
     input_path = os.path.join(current_dir, input_video_path)
-    command = ["ffprobe", "-i", input_path]
+    command = [ffprobe_path, "-i", input_path]
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
     # Mencari 'Stream #x:x' dengan 'Subtitle' di output FFmpeg
@@ -149,7 +151,7 @@ def create_hardsub_video(input_video_path, output_video_path):
     subtitle_path = os.path.join(current_dir, *video_input_path)
     input_path = os.path.join(current_dir, input_video_path)
     output_path = os.path.join(current_dir, output_video_path)
-    command = ['ffmpeg', '-hide_banner', "-threads", ffmpeg_threads, "-i", input_path, "-threads", ffmpeg_threads, "-vf", "subtitles='{}'".format(subtitle_path), '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-crf', '18', '-preset', 'fast', '-c:a', 'copy', '-movflags', '+faststart', '-sn', '-map_metadata', '-1', '-map_chapters', '-1', output_path]
+    command = [ffmpeg_path, '-hide_banner', "-threads", ffmpeg_threads, "-i", input_path, "-threads", ffmpeg_threads, "-vf", "subtitles='{}'".format(subtitle_path), '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-crf', '18', '-preset', 'fast', '-c:a', 'copy', '-movflags', '+faststart', '-sn', '-map_metadata', '-1', '-map_chapters', '-1', output_path]
     subprocess.run(command)
 
 # Fungsi untuk membuat folder pada Google Drive
@@ -209,10 +211,10 @@ def upload_file_to_gdrive(file_path, file_name, folder_id):
                     # Coba ulang upload lagi jika gagal
                     request = service.files().create(body=file_metadata, media_body=media, fields='id')
                 else:
-                    print("Upload gagal setelah {} kali percobaan. Error: {}".formt(max_retries, e))
+                    print("Upload gagal setelah {} kali percobaan. Error: {}".format(max_retries, e))
                     return None  # Gagal setelah semua retry
             except Exception as e:
-                print("Gagal mengupload file '{}': {}".formt(file_name, e))
+                print("Gagal mengupload file '{}': {}".format(file_name, e))
                 return None
     return None
 
